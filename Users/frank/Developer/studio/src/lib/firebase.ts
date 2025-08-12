8342ca403e59dd6c9837cb8415f319d18ec39309
Maximos Christoforidis <mirror@mirrornet.net>
2025-08-12 19:52:54 +0000

    studio-9409595598:~/studio{main}$ git push https://github.com/FrankDRabb

--- Users/frank/Developer/studio/src/lib/firebase.ts
+++ Users/frank/Developer/studio/src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, FirebaseApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
@@ -8,13 +7,13 @@ import { getStripePayments } from "@stripe/firestore-stripe-payments";
 
 // Your new Firebase project configuration.
 const firebaseConfig = {
-  apiKey: "AIzaSyCWa7WdbskdsTaGvxs1ORDEYNdKCijAHvc",
-  authDomain: "mirrornet-96cce.firebaseapp.com",
-  projectId: "mirrornet-96cce",
-  storageBucket: "mirrornet-96cce.firebasestorage.app",
-  messagingSenderId: "277723871042",
-  appId: "1:277723871042:web:ccf266035c41c65e3c7adb",
-  measurementId: "G-5WHB8YGR4T"
+  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY",
+  authDomain: "mirrornetv2.firebaseapp.com",
+  projectId: "mirrornetv2",
+  storageBucket: "mirrornetv2.appspot.com",
+  messagingSenderId: "715585417729",
+  appId: "1:715585417729:web:19a5b571a86bc181a98600",
+  measurementId: "G-LJW6HK1XBP"
 };
 
 // A more robust way to initialize Firebase, especially for server-side code.
